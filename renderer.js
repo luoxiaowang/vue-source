121c6b9ba612f35208eb231061c527f37cabf814
lance <15927510358@163.com>
2023-03-18 21:58:49 +0800

    basic

--- renderer.js
+++ renderer.js
@@ -1,8 +1,22 @@
 function createRenderer() {
+  function mountElement(vnode, container) {
+
+  }
   function patch(n1, n2, container) {
     // 
   }
-  function render() {
-    
+  function render(vnode, container) {
+    if(vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        container.innerHTML = ''
+      }
+    }
+     container._vnode = vnode
+  }
+
+  return {
+    render,
   }
 }
