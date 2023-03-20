3c7576c5549a3871ed924a98c76ec72296bfe547
lance <15927510358@163.com>
2023-03-20 10:34:36 +0800

    property处理

--- renderer.js
+++ renderer.js
@@ -9,9 +9,38 @@ function createRenderer(options) {
     const el = createElement(vnode.type)
     if (typeof vnode.children === 'string') {
       setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
     }
+
+    // 如果存在props时的处理
+    if (vnode.props) {
+      for(const key in vnode.props) {
+        // 无论是使用 setAttribute 函数，还是直接设置元素的 DOM Properties，都存在缺陷，我们只能做特殊处理
+        // 即优先设置元素的 DOM Properties，但当值为空字符串时，要手动将值矫正为 true
+        // key是否存在对应的DOM Properties
+        if (key in el) {
+          const type = typeof el[key]
+          const value = vnode.props[key]
+
+          // 这种情况特殊处理，例如模板中的disabled属性设置
+          if (type === 'boolean' && value === '') {
+            el[key] = true
+          } else {
+            el[key] = value
+          }
+        } else {
+          // setAttribute设置的值总是会被字符串化
+          el.setAttribute(key, vnode.props[key])
+        }
+      }
+    }
+
     insert(el, container)
   }
+
   function patch(n1, n2, container) {
     // 如果n1不存在，则意味着是挂载
     if (!n1) {
@@ -20,6 +49,7 @@ function createRenderer(options) {
       // patch
     }
   }
+
   function render(vnode, container) {
     if (vnode) {
       patch(container._vnode, vnode, container)
@@ -37,9 +67,18 @@ function createRenderer(options) {
 }
 
 const vnode = {
-  type: 'h1',
-  children: 'hello',
+  type: 'div',
+  props: {
+    id: 'foo',
+  },
+  children: [
+    {
+      type: 'p',
+      children: 'hello',
+    }
+  ],
 }
+
 // 创建一个渲染器
 const renderer = createRenderer({
   createElement(tag) {
@@ -52,4 +91,5 @@ const renderer = createRenderer({
     parent.insertBefore(el, anchor)
   },
 })
+
 renderer.render(vnode, document.querySelector('#app'))
