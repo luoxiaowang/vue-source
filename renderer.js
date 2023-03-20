be105f1448bc7c7cad825d98937d2dec2ba2c1a8
lance <15927510358@163.com>
2023-03-20 09:17:20 +0800

    demo

--- renderer.js
+++ renderer.js
@@ -1,9 +1,24 @@
-function createRenderer() {
-  function mountElement(vnode, container) {
+function createRenderer(options) {
+  const {
+    createElement,
+    setElementText,
+    insert,
+  } = options
 
+  function mountElement(vnode, container) {
+    const el = createElement(vnode.type)
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    }
+    insert(el, container)
   }
   function patch(n1, n2, container) {
-    // 
+    // 如果n1不存在，则意味着是挂载
+    if (!n1) {
+      mountElement(n2, container)
+    } else {
+      // patch
+    }
   }
   function render(vnode, container) {
     if (vnode) {
@@ -26,5 +41,15 @@ const vnode = {
   children: 'hello',
 }
 // 创建一个渲染器
-const renderer = createRenderer()
+const renderer = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag)
+  },
+  setElementText(el, text) {
+    el.textContent = text
+  },
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor)
+  },
+})
 renderer.render(vnode, document.querySelector('#app'))
